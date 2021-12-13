60effc4243277bd48967cdf88c8316c4dba7cd63
Brayan Gamboa <bsgv2005@gmail.com>
2021-12-13 02:43:03 -0500

    13/12/2021 - 2:43 A.M

--- logic.js
+++ logic.js
@@ -1,98 +1,101 @@
-let descript = "Hola Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus totam laudantium reiciendis modi soluta et tempora in similique? Excepturi quia quidem maxime reprehenderit magni, ut veritatis libero repellendus impedit expedita?";
-let productMoto = ""
+let descript =
+  "Hola Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus totam laudantium reiciendis modi soluta et tempora in similique? Excepturi quia quidem maxime reprehenderit magni, ut veritatis libero repellendus impedit expedita?";
+let productMoto = "";
 
+//Creo una moto con todas sus especificaciones
 let CRYPTON_FI = {
   nombre: "CRYPTON FI",
   precio: Number(6500000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/verde.png"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/verde.png",
 };
 let Xmax_300 = {
   nombre: "Xmax 300",
   precio: Number(25500000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2019/09/xmax3.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2019/09/xmax3.jpg",
 };
 let NMAX_155 = {
   nombre: "NMAX 155",
   precio: Number(11500000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/nmax-colores-20202-azul.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/nmax-colores-20202-azul.jpg",
 };
 let SZRR_150 = {
   nombre: "SZRR 150",
   precio: Number(9090000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/sz_gris_verde-1.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/sz_gris_verde-1.jpg",
 };
 let YBRZ_125 = {
   nombre: "YBRZ 125",
   precio: Number(7000000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2021/03/azul.png"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2021/03/azul.png",
 };
 let FZ_VERSION_2_150 = {
   nombre: "FZ VERSIÓN 2 150",
   precio: Number(8800000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/fz-azul.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/fz-azul.jpg",
 };
 let FZ25_250 = {
   nombre: "FZ25 250",
   precio: Number(12500000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2019/08/fz25_negra.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2019/08/fz25_negra.jpg",
 };
 let FZ_VERSION_2 = {
   nombre: "FZ VERSIÓN 2.0",
   precio: Number(8800000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/fz-gris.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/fz-gris.jpg",
 };
 let YCZ_110 = {
   nombre: "YCZ 110",
   precio: Number(5300000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/08/negro-rojo.png"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/08/negro-rojo.png",
 };
-/*Motos Enduro*/
+//Motos Enduro
 let YZ_450F = {
   nombre: "YZ-450F",
   precio: Number(9190000),
-  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ_450f_1.jpg"
+  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ_450f_1.jpg",
 };
 let YZ_250F = {
   nombre: "YZ-250F",
   precio: Number(8590000),
-  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ-250F-2-7.jpg"
+  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ-250F-2-7.jpg",
 };
 let YZ_250_2T = {
   nombre: "YZ-250 (2T)",
   precio: Number(7190000),
-  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ-250F-2-7.jpg"
+  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ-250F-2-7.jpg",
 };
 let WR_250F = {
   nombre: "WR-250F",
   precio: Number(9090000),
-  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ_450f_1.jpg"
+  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/10/YZ_450f_1.jpg",
 };
 let XTZ_150 = {
   nombre: "XTZ-150",
   precio: Number(8599000),
-  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/06/XTZ-01-negra.jpg"
+  url: "https://www.yamahamotos.cl/wp-content/uploads/2019/06/XTZ-01-negra.jpg",
 };
 let XTZ_125 = {
   nombre: "XTZ-125",
   precio: Number(6390000),
-  url: "https://www.yamahamotos.cl/wp-content/uploads/2018/07/blanco-1.jpg"
+  url: "https://www.yamahamotos.cl/wp-content/uploads/2018/07/blanco-1.jpg",
 };
 let XTZ_250 = {
   nombre: "XTZ 250",
   precio: Number(19000000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2019/11/xtz250-2020-blanca.png"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2019/11/xtz250-2020-blanca.png",
 };
 let YZ_85LW = {
   nombre: "YZ 85LW",
   precio: Number(7090000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/2021_YZ85LW_DPBSE_JPN.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/2021_YZ85LW_DPBSE_JPN.jpg",
 };
 let TTR_110 = {
   nombre: "TTR 110",
   precio: Number(11500000),
-  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/ttr110E.jpg"
+  url: "https://www.incolmotos-yamaha.com.co/wp-content/uploads/2018/03/ttr110E.jpg",
 };
 
+//Creo una function para llamar u obtener los valosres de cada moto
 function _TTR_110() {
   productMoto = TTR_110;
   console.log(productMoto);
@@ -104,8 +107,8 @@ function _XTZ_250() {
   productMoto = XTZ_250;
 }
 function _YZ_450F() {
-  productMoto = YZ_450F;
-  console.log(productMoto);
+  productMoto = JSON.stringify(YZ_450F);
+  console.log("HOLA");
 }
 function _YZ_250F() {
   productMoto = YZ_250F;
@@ -123,7 +126,7 @@ function _XTZ_125() {
   productMoto = XTZ_125;
 }
 
-/*Urbanas*/
+//Urbanas
 
 function _CRYPTON_FI() {
   productMoto = CRYPTON_FI;
@@ -153,20 +156,26 @@ function _YCZ_110() {
   productMoto = YCZ_110;
   console.log("Hola");
 }
-/*
-function producto() {
-  localStorage.setItem("moto2", JSON.stringify(CRYPTON_FI));
-}
-
-function product() {
-  console.log(CRYPTON_FI["precio"]);
-}
-
-function capturarDatos() {
-  let hole = JSON.parse(localStorage.getItem("moto2"));
-  console.log(hole[nombre]);
-}*/
-
+//Tengo un pequeño error a la hora de llmara la función desde la hoja externa, pero si lo hago desde esta hoja la lee completamente y sin problema.
+_XTZ_250()
+console.log(productMoto);
 precio.textContent = "$ " + productMoto["precio"];
 nombre.textContent = "Nombre: " + productMoto["nombre"];
-descripcion.textContent = "" + descript
+enlace.textContent = "Este es el enlace para poder ver la imagen del producto: " + productMoto["url"];
+descripcion.textContent = "" + descript;
+
+function pago() {
+  alert("El pago fue exitoso")
+}
+function obtenerDato() {
+let numeroMotos = Number(document.getElementById("cantidad").value);
+  //Me aseguro que el número sea positivo, lo convierto
+  if (numeroMotos < 0) {
+    numeroMotos = numeroMotos * -1;
+    alert("Ya hemos hecho la conversión a un número positivo");
+  }
+
+  valorAPagar = numeroMotos * productMoto["precio"];
+  console.log(valorAPagar);
+  cantidad.textContent = numeroMotos;
+}
